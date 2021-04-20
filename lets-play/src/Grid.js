12198c20c7aa93cc228467b3d01ed0734b18114b
Simone Ballard <simone.a.ballard@gmail.com>
2021-04-19 21:30:30 -0600

    grid is clickable

--- lets-play/src/Grid.js
+++ lets-play/src/Grid.js
@@ -1,16 +1,94 @@
 import React from 'react';
 import './Grid.css';
 
-const CELL_SIZE = 20;const WIDTH = 800;const HEIGHT = 600;
+const CELL_SIZE = 20;
+const WIDTH = 800;
+const HEIGHT = 600;
 
-class Game extends React.Component {
+class Cell extends React.Component {
     render() {
+        const { x, y } = this.props;
         return (
-            <>
-            <div className="Grid" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}></div>
-            </>
-        )
+            <div className="Cell" style={{ 
+                left: `${CELL_SIZE * x + 1}px`,
+                top: `${CELL_SIZE * y + 1}px`,
+                width: `${CELL_SIZE - 1}px`,
+                height: `${CELL_SIZE - 1}px`,
+            }}
+        />
+    );
     }
 }
 
-export default Game;
+class Grid extends React.Component {
+    constructor() {
+        super();
+        this.rows = HEIGHT/CELL_SIZE;
+        this.cols = WIDTH/CELL_SIZE;
+        this.grid = this.makeEmptyGrid();
+    }
+    state = { cells: [], }
+
+    makeEmptyGrid() {
+        let grid = [];
+        for (let y = 0; y < this.rows; y++) {
+            grid[y] = [];
+            for (let x = 0; x < this.cols; x++) {
+                grid[y][x] = false;
+            }
+        }
+        return grid;
+    }
+
+    makeCells() {
+        let cells = [];
+        for (let y = 0; y < this.rows; y++) {
+            for (let x = 0; x < this.cols; x++) {
+                if (this.grid[y][x]) {
+                    cells.push({ x, y });
+                }    
+            }
+        }
+        return cells; 
+    }
+
+    getElementOffset() {
+        const rect = this.gridRef.getBoundingClientRect();
+        const doc = document.documentElement;
+
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) -  doc.clientTop,
+        };
+    }
+
+    handleClick = (event) => {
+        const elemOffset = this.getElementOffset();
+        const offsetX = event.clientX - elemOffset.x;
+        const offsetY = event.clientY - elemOffset.y;
+
+        const x = Math.floor(offsetX / CELL_SIZE);
+        const y = Math.floor(offsetY / CELL_SIZE);
+
+        if (x >= 0 && x <= this.cols && y >= 0 && y<= this.rows) {
+            this.grid[y][x] = !this.grid[y][x];
+        }
+        this.setState({ cells: this.makeCells() });
+    }
+        
+    render() {
+        const { cells } = this.state;
+
+        return (
+            <>
+            <div className="Grid" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}} onClick={this.handleClick} ref={(n) => { this.gridRef = n; }}>
+                {cells.map(cell => (
+                    <Cell x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`}/>
+                ))}
+            </div>
+            </>
+        );
+    }
+}
+
+export default Grid;
